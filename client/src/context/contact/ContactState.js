5ad6ec0e546735eeeab06ddd454d57c3b31c00aa
Kanishk Gupta <kanishkgupta2000@gmail.com>
2019-08-16 00:38:29 +0530

    proxy fix

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -33,7 +33,7 @@ const ContactState=props=>{
 		
 
 		try {
-			const res=await axios.get('http://localhost:5000/api/contacts');
+			const res=await axios.get('/api/contacts');
 			dispatch({type:GET_CONTACTS,payload:res.data});
 		} catch (error) {
 			dispatch({type:CONTACT_ERROR,
@@ -53,7 +53,7 @@ const ContactState=props=>{
 		}
 
 		try {
-			const res=await axios.post('http://localhost:5000/api/contacts',contact,config)
+			const res=await axios.post('/api/contacts',contact,config)
 			dispatch({type:ADD_CONTACT,payload:res.data});
 		} catch (error) {
 			dispatch({type:CONTACT_ERROR,
@@ -71,7 +71,7 @@ const updateContact=async contact=>{
 	}
 
 	try {
-		const res=await axios.put(`http://localhost:5000/api/contacts/${contact._id}`,contact,config)
+		const res=await axios.put(`/api/contacts/${contact._id}`,contact,config)
 		dispatch({type:UPDATE_CONTACT,payload:res.data});
 	} catch (error) {
 		dispatch({type:CONTACT_ERROR,
@@ -85,7 +85,7 @@ const updateContact=async contact=>{
 	
 const deleteContact=async id=>{
 	try {
-		await axios.delete(`http://localhost:5000/api/contacts/${id}`)
+		await axios.delete(`/api/contacts/${id}`)
 		dispatch({type:DELETE_CONTACT,payload:id});
 	} catch (error) {
 		dispatch({type:CONTACT_ERROR,
